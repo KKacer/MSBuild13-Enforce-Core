1087f50a9474cdeb27ddba0c44f67ed3e65c4566
kasra.kacer <kasra.kacer@outlook.com>
2020-06-24 14:24:38 -0400

    Add project files.

---
+++ Consumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsumerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ NugLibEnforceCore/TestTask.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace ProDevelopers.ProBuild.Tasks
+{
+    public class TestTask : Task
+    {
+
+        public override bool Execute()
+        {
+            Log.LogMessage(importance: MessageImportance.High, "==== MHM: We are currently in the custom task! ");
+            Process.Start("notepad.exe");
+            return true;
+        }
+    }
+}
